c1fbd2ef59f7d2773fc55aa097687512acdabf67
jasmin <jasmin.software9@gmail.com>
2026-03-04 23:58:00 -0800

    set up app svc deployment

--- Demo.AppHost/AppHost.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var server = builder.AddProject<Projects.Server>("server");
-
-var blazor = builder.AddProject<Projects.Client>("blazor")
-    .WithEnvironment("AGUI_SERVER_URL", server.GetEndpoint("http"));
-builder.Build().Run();
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,55 +1,75 @@
 ﻿using System.ClientModel;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Agents.AI.Hosting.AGUI.AspNetCore;
 using Microsoft.Extensions.AI;
 using OpenAI;
 using OpenAI.Chat;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddHttpClient().AddLogging();
-builder.Services.AddAGUI();
-var app = builder.Build();
-
-app.Use(async (context, next) =>
+try
 {
-    var logger = context.RequestServices
-                        .GetRequiredService<ILoggerFactory>()
-                        .CreateLogger("AGUI.RequestLogger");
+        
 
-    logger.LogInformation(@"    Time: {Time}
-    Request Path: {Path}
-    Request Method: {Method}
-    Response Status Code: {StatusCode}
-    ",
-    DateTime.UtcNow,
-    context.Request.Path, 
-    context.Request.Method, 
-    context.Response.StatusCode);
+    var builder = WebApplication.CreateBuilder(args);
 
-    await next();
-});
+    builder.Services.AddHttpClient().AddLogging();
+    builder.Services.AddAGUI();
 
-string? apiKey = builder.Configuration["GitHub:Token"];
-string? endpoint = builder.Configuration["GitHub:ApiEndpoint"] ?? "https://models.github.ai/inference";
-string? deploymentName = builder.Configuration["GitHub:Model"] ?? "openai/gpt-4o-mini";
+    builder.Services.AddControllers();
 
-// Create AI agent
-ChatClient chatClient = new OpenAIClient(
-    new ApiKeyCredential(apiKey!),
-    new OpenAIClientOptions()
+    var app = builder.Build();
+
+    app.UseBlazorFrameworkFiles();
+    app.UseStaticFiles();
+    app.MapControllers();
+    app.MapFallbackToFile("index.html");
+
+
+    app.Use(async (context, next) =>
     {
-        Endpoint = new Uri(endpoint)
-    })
-    .GetChatClient(deploymentName);
+        var logger = context.RequestServices
+                            .GetRequiredService<ILoggerFactory>()
+                            .CreateLogger("AGUI.RequestLogger");
 
-var assistantAgent = chatClient.CreateAIAgent(instructions: @"
-    You are a professional productivity assistant that helps manage my workspace.");
+        logger.LogInformation(@"    Time: {Time}
+        Request Path: {Path}
+        Request Method: {Method}
+        Response Status Code: {StatusCode}
+        ",
+        DateTime.UtcNow,
+        context.Request.Path, 
+        context.Request.Method, 
+        context.Response.StatusCode);
 
-var friendlyAgent = chatClient.CreateAIAgent(instructions: @"
-    You are a good friend who is bubbly, speaks casually, and uses with a lot of emojis. You provide support and encouragement.");
+        await next();
+    });
 
-// Map the AG-UI agent endpoint
-app.MapAGUI("/assistant", assistantAgent);
-app.MapAGUI("/friend", friendlyAgent);
+    string? apiKey = builder.Configuration["GitHub:Token"];
+    string? endpoint = builder.Configuration["GitHub:ApiEndpoint"] ?? "https://models.github.ai/inference";
+    string? deploymentName = builder.Configuration["GitHub:Model"] ?? "openai/gpt-4o-mini";
 
-app.Run();
+    // Create AI agent
+    ChatClient chatClient = new OpenAIClient(
+        new ApiKeyCredential(apiKey!),
+        new OpenAIClientOptions()
+        {
+            Endpoint = new Uri(endpoint)
+        })
+        .GetChatClient(deploymentName);
+
+    var assistantAgent = chatClient.CreateAIAgent(instructions: @"
+        You are a professional productivity assistant that helps manage my workspace.");
+
+    var friendlyAgent = chatClient.CreateAIAgent(instructions: @"
+        You are a good friend who is bubbly, speaks casually, and uses with a lot of emojis. You provide support and encouragement.");
+
+    // Map the AG-UI agent endpoint
+    app.MapAGUI("/assistant", assistantAgent);
+    app.MapAGUI("/friend", friendlyAgent);
+
+    app.Run();
+}
+catch (Exception ex)
+{    
+    Console.WriteLine($"An error occurred: {ex.Message}");
+    throw;
+}
